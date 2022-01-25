b6a9a7a71197ab70d46547a68b1039bb28f49072
UnitedCatdom <78739707+CuteBladeYT@users.noreply.github.com>
2022-01-25 17:26:47 +0100

    Create F4 Lobby Change.js

---
+++ F4 Lobby Change.js
@@ -0,0 +1,14 @@
+document.addEventListener("keyup", (event) => {
+  if (localStorage["uci_changeLobbyKey"])) {} else {
+    localStorage["uci_changeLobbyKey"] = "F4";
+  };
+  if (localStorage["uci_changeLobbyKey"] == "") {
+    localStorage["uci_changeLobbyKey"] = "F4";
+  };
+
+  let clk = localStorage["uci_changeLobbyKey"];
+  let pk = event.key;
+  if (pk == clk) {
+    window.location.href = "https://krunker.io/";
+  };
+})
